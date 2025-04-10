e02d30bcb61f95a764ce0e13f3ba1a1a73e037f2
SaharOpmed <sahar@opmed.ai>
2025-04-10 16:32:00 +0300

    Create custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter basename="/opmed-dashboard">
+  <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
