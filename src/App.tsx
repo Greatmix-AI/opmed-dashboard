ed4b7e536f5d426be64adc1ce8dad1e02ebd7f92
SaharOpmed <sahar@opmed.ai>
2025-04-10 16:12:52 +0300

    fix 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/opmed-dashboard">
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
